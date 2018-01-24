29343225c0433d84a725f03ea550ba7fa55c43b2
Sacrimento <abouvero@student.42.fr>
2018-01-24 14:45:51 +0100

    Protected strcmp

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 16:46:37 by abouvero          #+#    #+#             */
-/*   Updated: 2018/01/22 20:18:53 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/01/24 13:17:20 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strcmp(const char *s1, const char *s2)
 	int		i;
 
 	i = 0;
+	if (!s1)
+		return (-1);
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
